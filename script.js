e1b9adf132bd9d7ab67f09a02fc64e04e460c382
Pratyaksh Suri <115720372+pratyakshsuri2003@users.noreply.github.com>
2023-11-01 11:44:51 +0530

    script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["SWE Intern", "@ Mfilterit", "Python Developer", "Data Science Intern"],
+        strings: ["Data Scientist", "Ex Data Science Intern @Mfilterit", "Python Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["SWE Intern", "@ Mfilterit", "Python Developer", "Data Science Intern"],
+        strings: ["Data Scientist", "Ex Data Science Intern @Mfilterit", "Python Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
